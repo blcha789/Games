7ded89008c1969158f366948c2fdf02a60106642
blcha789 <51416949+blcha789@users.noreply.github.com>
2019-10-01 08:14:34 +0200

    Update CharacterStats.cs

--- SpellForce/Assets/Scripts/Game/CharacterStats.cs
+++ SpellForce/Assets/Scripts/Game/CharacterStats.cs
@@ -4,35 +4,105 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    [Header("Start Stats")]
+	public float starthealth = 100f;
+	public float startMana = 100f;	
+	public float startHealthRegen = 1f;
+	public float startManaRegen = 1f;
+	
+	[Header("Modifiers")]
+	public float healthModifier = 10;
+	public float manaModifier = 10;
+	public float healthRegenModifier = 0.2f;
+	public float manaRegenModifier = 0.2f; 
 
-    public float startMana = 100;
-    public float manaModifier = 10;
+	private float currentManaRegen;
+	private float currentHealthRegen;
 
-    [Header("Mana")]
-    public float fireMana;
-    public float waterMana;
-    public float coldMana;
-    public float airMana;
-    public float darkMana;
-    public float earthMana;
+	private float currentHealth;
+	private float currentFireMana;
+    private float currentWaterMana;
+    private float currentColdMana;
+    private float currentAirMana;
+    private float currentDarkMana;
+    private float currentEarthMana;
 
-    void Start()
+	private float maxHealth;
+	private float maxFireMana;
+    private float maxWaterMana;
+    private float maxColdMana;
+    private float maxAirMana;
+    private float maxDarkMana;
+    private float maxEarthMana;                    
+
+	private bool isDead = false;
+
+    private void Start()
     {
-        SetMana();
+        SetStats();
     }
 
-    private void SetMana()
+    private void SetStats()
     {
-        fireMana = startMana + (PlayerPrefs.GetInt("FireManaModifier") * manaModifier);
-        waterMana = startMana + (PlayerPrefs.GetInt("WaterManaModifier") * manaModifier);
-        coldMana = startMana + (PlayerPrefs.GetInt("ColdManaModifier") * manaModifier);
-        airMana = startMana + (PlayerPrefs.GetInt("AirManaModifier") * manaModifier);
-        darkMana = startMana + (PlayerPrefs.GetInt("DarkManaModifier") * manaModifier);
-        earthMana = startMana + (PlayerPrefs.GetInt("EarthManaModifier") * manaModifier);
-    }
-
-    void Update()
-    {
+        maxHealth = startHealth + PlayerPrefs.GetInt("HealthModifier") * healthModifier;
+	    currentHealth = maxHealth;
         
+        currentHealthRegen = startHealthRegen + PlayerPrefs.GetInt("HealthRegenModifier") * healthRegenModifier;
+	    currentManaRegen = startManaRegen + PlayerPrefs.GetInt("ManaRegenModifier") * manaRegenModifier;
+     
+        maxFireMana = startMana + (PlayerPrefs.GetInt("FireManaModifier") * manaModifier);
+        maxWaterMana = startMana + (PlayerPrefs.GetInt("WaterManaModifier") * manaModifier);
+        maxColdMana = startMana + (PlayerPrefs.GetInt("ColdManaModifier") * manaModifier);
+        maxAirMana = startMana + (PlayerPrefs.GetInt("AirManaModifier") * manaModifier);
+        maxDarkMana = startMana + (PlayerPrefs.GetInt("DarkManaModifier") * manaModifier);
+        maxEarthMana = startMana + (PlayerPrefs.GetInt("EarthManaModifier") * manaModifier);
+        
+        currentFireMana = maxFireMana;
+        currentWaterMana = maxWaterMana;
+        currentColdMana = maxColdMana;
+        currentAirMana = maxAirMana;
+        currentDarkMana = maxDarkMana;
+        currentEarthMana = maxEarthMana;
     }
+
+    private void Update()
+	{
+	 Regeneration();
+	}
+
+
+	private void Regeneration()
+	{
+	  if(currentHealth < maxHealth)
+	  {
+	   currentHealth += currentHealthRegen * Time.deltaTime;
+	   //UI
+	  }
+
+	  if(currentMana < maxMana)
+	  {
+	   currentMana += currentManaRegen * Time.delatTime;
+ 	   //UI
+	  }	
+	}
+
+	public void TakeDamage(float damage)
+	{
+	  if(isDead)
+	    return;
+
+	  currentHealth -= damage;
+	  //UI
+
+	  if (currentHealth <= 0)
+          {
+	   //GameOver
+	  }
+	}
+
+	public void TakeMana(float mana) 
+	{
+	 currentMana -= mana;
+	 //UI
+	}
 }
