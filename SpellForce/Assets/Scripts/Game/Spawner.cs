58413d88ca4b3ff42e2cdc0d20afc01af8db708f
blcha789 <51416949+blcha789@users.noreply.github.com>
2019-10-10 18:33:54 +0200

    add

--- SpellForce/Assets/Scripts/Game/Spawner.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu (menuName = "SceneSettings/Spawner", order = 1)]
-public class Spawner : ScriptableObject
-{
-    public int amount;
-}
