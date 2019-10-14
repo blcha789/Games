ec6e2ef63d55ea28a9ccf4e5efc2ffadbef5cf6f
blcha789 <51416949+blcha789@users.noreply.github.com>
2019-10-14 09:43:01 +0200

    Create EnemyMovement.cs

---
+++ SpellForce/Assets/Scripts/Game/EnemyMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private Transform player;
+    private Rigidbody rb;
+
+    private bool isStuned = false;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Character").transform;
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    public void Stun(bool _isStuned)
+    {
+      isStuned = _isStuned;
+    }
+
+    public void Slow(float slowSpeed)
+    {
+       movementSpeed += slowSpeed;
+    }
+
+    void FixedUpdate()
+    {
+      if(!isStuned)
+      {        
+        transform.LookAt(player.position);
+        Vector3 direction = (player.position - transform.position).normalized;
+        rb.MovePosition(transform.position + direction * movementSpeed * Time.deltaTime);
+      }
+   }
+}
