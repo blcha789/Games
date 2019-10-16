e8740bc47321e078a18a891709fb0e9556544bb0
blcha789 <51416949+blcha789@users.noreply.github.com>
2019-10-16 20:48:01 +0200

    add

---
+++ SpellForce/Assets/Scripts/Game/Enemy/EnemyShooting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public bool silence = false;
+
+    public float attackDistance;
+    public float damage;
+    public float attackSpeed;
+
+    private bool canAttack = true;
+    private float currentAttackSpeed;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Character").transform;
+        //anim = GetComponentInChildren<Animator>();
+        currentAttackSpeed = attackSpeed;
+    }
+
+    void Update()
+    {
+        if (currentAttackSpeed <= 0)
+        {
+            canAttack = true;
+            currentAttackSpeed = attackSpeed;
+        }
+        else
+        {
+            if (!canAttack)
+                currentAttackSpeed -= Time.deltaTime;
+        }
+
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (canAttack && !silence)
+        {
+            if (distance <= attackDistance)
+            {
+                //AttackPlayer();
+                //attackSound.Play();
+            }
+        }
+    }
+}
---
+++ SpellForce/Assets/Scripts/Game/Enemy/EnemyStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float health;
+    BuffSystem buffSystem;
+
+    void Start()
+    {
+        buffSystem = GetComponent<BuffSystem>();
+    }
+
+    public void TakeDamagePerHit(float damage)
+    {
+        //buffSystem.AddBuff();
+        TakeDamage(damage);
+    }
+
+    public void TakeDamagePerSecond(float damage)
+    {
+        TakeDamage(damage);
+    }
+
+    private void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+            Death();
+    }
+
+    private void Death()
+    {
+
+    }
+}
