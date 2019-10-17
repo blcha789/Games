7d9278f807f130ff21cb34c5bee7ec40ebd4d99a
blcha789 <51416949+blcha789@users.noreply.github.com>
2019-10-17 12:50:46 +0200

    Update EnemyMovement.cs

--- SpellForce/Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ SpellForce/Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movementSpeed;
+
+    private Transform player;
+    private Rigidbody rb;
+
+    private bool isStuned = false;
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Character").transform;
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    public void MovementSettings(float minMoveSpeed, float maxMoveSpeed)
+    {
+        movementSpeed = Random.Range(minMoveSpeed, maxMoveSpeed);
+    }
+    
+    public void Stun(bool _isStuned)
+    {
+      isStuned = _isStuned;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Slow(float slowSpeed)
     {
-        
+       movementSpeed += slowSpeed;
     }
+
+    void FixedUpdate()
+    {
+      if(!isStuned)
+      {        
+        transform.LookAt(player.position);
+        Vector3 direction = (player.position - transform.position).normalized;
+        rb.MovePosition(transform.position + direction * movementSpeed * Time.deltaTime);
+      }
+   }
 }
